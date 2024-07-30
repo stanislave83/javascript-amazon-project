642c7dcfbec6ad89d188dfde61cfa436c4c8833a
StaniSlame <kozlovfstanislav@gmail.com>
2024-07-30 14:26:21 +0700

    Learn error handling

--- data/products.js
+++ data/products.js
@@ -131,9 +131,12 @@ export function loadProductsFetch(){
       });
 
       console.log('load products');
+  }).catch((error)=>{
+    console.log('Unexpected error. Please try again later');
   });
   return promise;
 }
+
 /*
 loadProductsFetch().then(()=>{
   console.log('next step')
@@ -159,6 +162,10 @@ export function loadProducts(fun){
 
       fun();
   });
+
+  xhr.addEventListener('error',(error)=>{
+    console.log('Unexpected error. Please try again later');
+  })
 
   xhr.open('GET', 'https://supersimplebackend.dev/products');
   xhr.send();
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -8,13 +8,20 @@ import { loadCart } from '../data/cart-class.js';
 // import '../data/backend-practice.js';
 
 async function loadPage(){
-  await loadProductsFetch();
+  try{
+    // throw 'error1';
+    await loadProductsFetch();
 
-  const value = await new Promise((resolve)=>{
-    loadCart(()=>{
-      resolve('value3');
+    const value = await new Promise((resolve,reject)=>{
+      // throw 'error2';
+      loadCart(()=>{
+        // reject('error3')
+        resolve('value3');
+      });
     });
-  })
+  }catch(error){
+    console.log('Unexpected error. Please try again later');
+  }
 
   renderCheckoutHeader();
   renderOrderSummary();
