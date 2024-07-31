b78daff5752f37c1d99b8821fac8cbc89b2cffab
stanislave83 <kozlovfstanislav@gmail.com>
2024-07-31 23:05:40 +0700

    Update cart-class.js

--- data/cart-class.js
+++ data/cart-class.js
@@ -19,7 +19,7 @@ class Cart {
         deliveryOptionId:'1'
       },{
         productId:'15b6fc6f-327a-4ec4-896f-486349e85a3d',
-        quantity:1,
+        quantity,
         deliveryOptionId:'2'
       }];
     }
